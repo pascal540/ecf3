e2f06a1f86e62a3ab2edb7f1acb117a4545e9a0e
pascal540 <marketing54320@gmail.com>
2022-01-20 16:38:28 +0100

    mairie +ajout des 2 formulaires

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Actualites;
+use App\Entity\Contact;
+use App\Entity\Evenement;
+use App\Form\ActualitesType;
+use App\Form\AdministrationType;
 use App\Repository\EvenementRepository;
+use App\Repository\ActualitesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,14 +20,55 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin")
      */
-    public function index(EvenementRepository $repo): Response
-    {
-        $evenements = $repo->findBy([], array('createdAt' => 'desc'));
+    public function ajout(
+        Request $request,
+        EntityManagerInterface $manager
+    ): Response {
+
+        $evenement = new Evenement();
+        $actualite = new Actualites();
+
+        $form = $this->createForm(AdministrationType::class, $evenement);
+        $form->handleRequest($request);
+
+        $form2 = $this->createForm(ActualitesType::class, $actualite);
+        $form2->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if (!$evenement->getId()) {
+                $evenement->setCreatedAt(new \DateTime());
+            }
+
+
+            $manager->persist($evenement);
+            $manager->flush();
+            return $this->redirectToRoute(
+                'admin',
+                ['id' => $evenement->getId()]
+            );
+        }
+        if ($form2->isSubmitted() && $form2->isValid()) {
+
+            if (!$actualite->getId()) {
+                $actualite->setCreatedAt(new \DateTime());
+            }
+
+
+            $manager->persist($actualite);
+            $manager->flush();
+            return $this->redirectToRoute(
+                'admin',
+                ['id' => $actualite->getId()]
+            );
+        }
 
         return $this->render('admin/admin.html.twig', [
             'controller_name' => 'AdminController',
-            'evenements' => $evenements,
-            'i' => 0
+            'form' => $form->createView(),
+            'form2' => $form->createView()
+
         ]);
     }
 }
--- src/Form/ActualitesType.php
+++ src/Form/ActualitesType.php
@@ -2,12 +2,12 @@
 
 namespace App\Form;
 
-use App\Entity\Evenement;
+use App\Entity\Actualites;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AdministrationType extends AbstractType
+class ActualitesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -20,7 +20,7 @@ class AdministrationType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Evenement::class,
+            'data_class' => Actualites::class,
         ]);
     }
 }
