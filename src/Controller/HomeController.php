b86a0dbe628547171804328cbb61b37266734071
pascal540 <marketing54320@gmail.com>
2022-01-25 10:13:42 +0100

    mairie accueil ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ActualitesRepository;
 use App\Repository\EvenementRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,13 +15,16 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(EvenementRepository $repo): Response
+    public function index(EvenementRepository $repo, ActualitesRepository $repoActu): Response
     {
-        $evenements = $repo->findBy([], array('createdAt' => 'DESC'), 2);
+        $evenements = $repo->findBy([], array('createdAt' => 'DESC'), 4);
+        $actus = $repoActu->findBy([], array('createdAt' => 'DESC'), 4);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'EvenementsController',
             'evenements' => $evenements,
-            'i' => 0
+            'i' => 1,
+            'actualites' => $actus,
+            'j' => 1
         ]);
 
         // return $this->render('home/index.html.twig', [
